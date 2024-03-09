9af17b9e648a365bf42e035a92b039249e1fd1fc
Zach3292 <Zach3292@hotmail.com>
2024-03-09 10:34:03 -0500

    basic template for dev 2

--- Devoir 2.py
+++ Devoir 2.py
@@ -1,11 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import norm
 
 def simpson(f, a, b, n) :
 
     # INSCRIRE DU CODE ICI
+    x = np.linspace(a, b, n+1)
+    y = f(x)
+    dx = (b - a) / n
 
-    return 0
+    integrale = np.sum(np.fromiter((
+    y[2*i] + 4*y[2*i+1] + y[2*i+2] for i in range(int(n/2))
+    ), dtype=float)) * dx / 3 # 3 parce que on a n/2 intervalles au lieu de n
+
+    return integrale
 
 # Spécifier la moyenne et l'écart type de la loi normale
 mu = float(input("Moyenne de la loi normale : "))
@@ -15,7 +23,7 @@ sigma = float(input("Écart type de la loi normale : "))
 
 # INSCRIRE DU CODE ICI
 # Vous devez changer la fonction pour celle décrivant une loi normale de moyenne mu et d'écart type sigma
-f = lambda x : x**2
+f = lambda x : norm.pdf(x, loc=mu, scale=sigma) # TODO : voir si c'est la bonne fonction
 
 # Graphique de la courbe associée à la loi normale
 x = np.linspace(mu-4 * sigma, mu+4 * sigma, 1000)
@@ -34,29 +42,37 @@ type_probabilite = int(input("Type de probabilité : "))
 while type_probabilite not in {1, 2, 3}:
     type_probabilite = int(input("Choisir le type 1, 2 ou 3 : "))
 
+erreur_maximale = 0.001
+
 if type_probabilite == 1:
     a = float(input("Valeur du a dans P(X < a) : "))
+    borne_inferieure = -np.inf
 
     # Évaluation de la probabilité
     if a < mu:
         p = 0
         # Calcul du nombre de sous intervalles nécessaire
-        
+
         # INSCRIRE DU CODE ICI
+        n = (( ((a - borne_inferieure) ** 5) / (180 * erreur_maximale) ) * val_max_dev_4) ** (1/4)
+        # TODO : demander au prof pour la borne inférieure et changer la valeur maximale de la dérivée 4ieme
 
         # Évaluation de la probabilité
         
         # INSCRIRE DU CODE ICI
-
+        p = simpson(f, borne_inferieure, a, n)
     else:
         p = 0
         # Calcul du nombre de sous intervalles nécessaire
 
         # INSCRIRE DU CODE ICI
-
+        n = (( ((a - borne_inferieure) ** 5) / (180 * erreur_maximale) ) * val_max_dev_4) ** (1/4)
+        # TODO : demander au prof pour la borne inférieure et changer la valeur maximale de la dérivée 4ieme
+        
         # Évaluation de la probabilité
         
         # INSCRIRE DU CODE ICI
+        p = simpson(f, borne_inferieure, a, n)
 
     print("La probabilité P(X < %.3f) vaut %.6f" % (a, p))
 
@@ -80,11 +96,13 @@ elif type_probabilite == 2:
     # Calcul du nombre de sous intervalles nécessaire
 
     # INSCRIRE DU CODE ICI
+    n = (( ((b - a) ** 5) / (180 * erreur_maximale) ) * val_max_dev_4) ** (1/4)
+    # TODO : changer la valeur maximale de la dérivée 4ieme
 
     # Évaluation de la probabilité
     
     # INSCRIRE DU CODE ICI
-
+    p = simpson(f, a, b, n)
     print("La probabilité P(%.3f < X < %.3f) vaut %.6f" % (a, b, p))
 
     # Tracer la région associée à la probabilité
@@ -98,27 +116,32 @@ elif type_probabilite == 2:
 
 elif type_probabilite == 3:
     a = float(input("Valeur du a dans P(X > a) : "))
-
+    borne_superieur = np.inf
     # Évaluation de la probabilité
     if a < mu:
         p = 0
         # Calcul du nombre de sous intervalles nécessaire
 
         # INSCRIRE DU CODE ICI
+        n = (( ((borne_superieur - a) ** 5) / (180 * erreur_maximale) ) * val_max_dev_4) ** (1/4)
+        # TODO : demander au prof pour la borne supérieure et changer la valeur maximale de la dérivée 4ieme
 
         # Évaluation de la probabilité
         
         # INSCRIRE DU CODE ICI
-
+        p = simpson(f, a, borne_superieur, n)
     else:
         p = 0
         # Calcul du nombre de sous intervalles nécessaire
-        
+
         # INSCRIRE DU CODE ICI
+        n = (( ((borne_superieur - a) ** 5) / (180 * erreur_maximale) ) * val_max_dev_4) ** (1/4)
+        # TODO : demander au prof pour la borne supérieure et changer la valeur maximale de la dérivée 4ieme
 
         # Évaluation de la probabilité
         
         # INSCRIRE DU CODE ICI
+        p = simpson(f, a, borne_superieur, n)
 
     print("La probabilité P(X > %.3f) vaut %.6f" % (a, p))
 
