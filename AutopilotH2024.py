95b597c249355b93589526f05be83589fdc770b7
Zach3292 <Zach3292@hotmail.com>
2024-03-26 22:39:48 -0400

    sans openCV et cours 13

--- AutopilotH2024.py
+++ AutopilotH2024.py
@@ -3,14 +3,112 @@ import skimage as ski
 import skvideo.io
 import matplotlib.pyplot as plt
 import cv2 as cv
+from scipy.stats import norm
+from scipy.signal import fftconvolve
 
 np.float = np.float64
 np.int = np.int_
 
+def gradient_image(img):
+    G = np.zeros((img.shape[0], img.shape[1], 2))
+    # TODO voir si je n'utilise pas des Sobel filters à la place pour approximer le gradient avec une convolution
+    # Ajouter du code ici
+    ddx = np.gradient(img, axis = 1)
+    ddy = np.gradient(img, axis = 0)
+    G[:, :, 0] = ddx
+    G[:, :, 1] = ddy
+
+    N = np.hypot(G[:,:,0], G[:,:,1])
+    N = N / N.max() * 255
+    theta = np.arctan2(G[:,:,1], G[:,:,0])
+    return N, theta
+
+def non_maximum_suppression(G, theta):
+    M, N = G.shape
+    Z = np.zeros((M,N), dtype=np.int32)
+    angle = theta * 180. / np.pi
+    angle[angle < 0] += 180
+
+    # Ajouter du code ici
+    for i in range(1, M-1):
+        for j in range(1, N-1):
+            
+            q = 255
+            r = 255
+
+            # Split en 8 directions
+            # if (0 <= angle[i,j] < 22.5) or (157.5 <= angle[i,j] <= 180):
+            #     q = G[i, j+1]
+            #     r = G[i, j-1]
+            # elif (22.5 <= angle[i,j] < 67.5):
+            #     q = G[i+1, j-1]
+            #     r = G[i-1, j+1]
+            # elif (67.5 <= angle[i,j] < 112.5):
+            #     q = G[i+1, j]
+            #     r = G[i-1, j]
+            # elif (112.5 <= angle[i,j] < 157.5):
+            #     q = G[i-1, j-1]
+            #     r = G[i+1, j+1]
+
+            # Split en 4 directions
+            # Semble mieux marcher pour déterminer les lignes dans ce cas-ci
+            if (0 <= angle[i,j] < 45) or (135 <= angle[i,j] <= 180):
+                q = G[i, j+1]
+                r = G[i, j-1]
+            elif (45 <= angle[i,j] < 135):
+                q = G[i+1, j]
+                r = G[i-1, j]
+            
+            if (G[i,j] >= q) and (G[i,j] >= r):
+                Z[i,j] = G[i,j]
+            else:
+                Z[i,j] = 0
+    return Z
+
+def hysteresis_double_thresholding(img, T1, T2):
+    M, N = img.shape
+    resulant = np.zeros((M,N), dtype=np.int32)
+
+    fort = np.int32(255)
+    faible = np.int32(25)
+
+    fort_i, fort_j = np.where(img >= T2)
+    faible_i, faible_j = np.where((img <= T2) & (img >= T1))
+
+    resulant[fort_i, fort_j] = fort
+    resulant[faible_i, faible_j] = faible
+
+    for i in range(1, M-1):
+        for j in range(1, N-1):
+            if resulant[i,j] == faible:
+                if ((resulant[i+1, j-1] == fort) or (resulant[i+1, j] == fort) or (resulant[i+1, j+1] == fort)
+                    or (resulant[i, j-1] == fort) or (resulant[i, j+1] == fort)
+                    or (resulant[i-1, j-1] == fort) or (resulant[i-1, j] == fort) or (resulant[i-1, j+1] == fort)
+                    ):
+                    resulant[i, j] = fort
+                else:
+                    resulant[i, j] = 0
+
+    return resulant
+
+def gaussian_kernel(size, sigma=1):
+    kernel_1D = np.linspace(-(size // 2), size // 2, size)
+    for i in range(size):
+        kernel_1D[i] = norm.pdf(kernel_1D[i], loc=0, scale=sigma)
+    kernel_2D = np.outer(kernel_1D.T, kernel_1D.T)
+ 
+    kernel_2D *= 1.0 / kernel_2D.max()
+
+    return kernel_2D
+
+def gaussian_blur(image, kernel_size):
+    kernel = gaussian_kernel(kernel_size, sigma=np.sqrt(kernel_size))
+    return fftconvolve(image, kernel, mode='same')
+
 def region(image, polygon):
-    
+
     mask = np.zeros_like(image)
-    
+
     mask = cv.fillPoly(mask, polygon, 255)
     mask = cv.bitwise_and(image, mask)
     return mask
@@ -30,24 +128,27 @@ for it in range(400, 500):
     
     # Affichage du numéro l'image traitée
     print(it)
+    polygon1 = np.array([
+                        [(100, 900), (100, 500), (1280, 500), (1280, 900)]
+                        ])
+    # Application d'un masque pour ne garder que les lignes de la route dans la région voulue
+    img_crop = region(img, polygon1)
+    gaussian = gaussian_blur(img_crop, 5)
+    grad, theta = gradient_image(gaussian)
+    max = non_maximum_suppression(grad, theta)
+    contour = hysteresis_double_thresholding(max, 10, 30)
 
-    # Application d'un filtre gaussien
-    gaussian = cv.GaussianBlur(img,(5,5),0)
-    
-    # Détection des contours avec OpenCV
-    contour = cv.Canny(gaussian, 10, 60, L2gradient=True)
-    
-    polygon = np.array([
+    polygon2 = np.array([
                         [(150, 750), (500, 520), (660, 500), (1000, 700), (1280, 800)]
                         ])
     # Application d'un masque pour ne garder que les lignes de la route dans la région voulue
-    C = region(contour, polygon)
+    C = region(contour, polygon2)
 
     mask = C
 
     # Pour débugger, décommentez les lignes suivantes
     # B = np.copy(contour)
-    # mask = cv.fillPoly(B, polygon, 255)
+    # mask = cv.fillPoly(B, polygon2, 255)
     
     # Données de gauche (modifiées pour améliorer la performance de l'algorithme)
     Cleft = np.copy(C)
